5043244ffc957701bdb4e506aab037c48abb68c0
fgn90 <guadagno.neto@gmail.com>
2022-10-11 12:57:03 -0300

    interação hora do dia

---
+++ ex010/Hora do dia/script.js
@@ -0,0 +1,24 @@
+function carregar() {
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+
+
+    msg.innerHTML = `Agora são ${hora} horas.`
+
+    if (hora >= 0 && hora < 12) {
+        img.src = 'img/morning.png'
+        document.body.style.background = '#B7782A'
+    } else if (hora >= 12 && hora < 18) {
+        img.src = 'img/afternoon.png'
+        document.body.style.background = '#FAAD42'
+    } else {
+        img.src = 'img/evening.png'
+        document.body.style.background = '#BE99BD'
+    }
+}
+
+//  Tarde #FAAD42
+//  Noite #BE99BD
+//  dia #AA5B07
