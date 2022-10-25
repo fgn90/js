d8dc47534de3ebe33157451630a70b9015121700
fgn90 <guadagno.neto@gmail.com>
2022-10-24 22:49:20 -0300

    att

---
+++ Udemy/aula 5/variaveis.js
@@ -0,0 +1,27 @@
+/*
+1 - Sempre recomendado utilizar let por ser mais moderno que var;
+2 - Não podem ser utilizadas palavras reservadas do JS para a criação de variáveis;
+3 - Colocar sempre nomes significativos para o código;
+4 - Não pode começar o nome de uma variável com um número;
+5 - Variáveis não podem conter traços ou espaços;
+6 - Para declarações compostas, é utilizado o modelo camelCase. Exemplo: 'nomeCompleto';
+7 - As variáveis são case-sensitive, ou seja, diferencia maiúsculas de minusculas. 
+8 - Não pode redeclarar variáveis com a 'let';
+*/
+
+let = nome = 'André'
+console.log(nome, 'nasceu em 1990.');
+console.log('Em 2000', nome, 'conheceu Claudia.');
+console.log(nome, 'casou-se com Claudia em 2020.');
+console.log('Claudia teve 1 filho com', nome, 'em 2021.');
+console.log('O filho de', nome , 'se chama Carlos.');
+
+//let nome; // Declaração da variável
+//nome = 'João';// Inicialização da variável
+//console.log(nome);
+//nome = 'Otávio';
+//console.log(nome);
+
+/*
+let nome; // Variáveis não podem ser redeclaradas.
+console.log('teste');*/
---
+++ Udemy/aula 6/constantes.js
@@ -0,0 +1,19 @@
+/*
+1 - Sempre recomendado utilizar const para a declaração de variáveis que não terão seu valor alterado ao longo do código;
+2 - Não podem ser utilizadas palavras reservadas do JS para a criação de constantes;
+3 - Colocar sempre nomes significativos para o código;
+4 - Não pode começar o nome de uma constantes com um número;
+5 - Constantes não podem conter traços ou espaços;
+6 - Para declarações compostas, é utilizado o modelo camelCase. Exemplo: 'nomeCompleto';
+7 - As constantes são case-sensitive, ou seja, diferencia maiúsculas de minusculas. 
+8 - Não pode redeclarar constantes;
+9 - Valores das constantes não podem seer modificados.
+*/
+
+//const nome = 'José';
+//console.log(nome);
+
+const n1 = 5;
+const n2 = 10;
+const res = n1 * n2;
+let resDuplicado = res * 3;
