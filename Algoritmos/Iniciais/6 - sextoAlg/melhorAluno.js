faf91eabbdb74078a08fc289bfd1afe1a997c582
fgn90 <guadagno.neto@gmail.com>
2023-01-26 11:39:12 -0300

    melhorAluno

--- Algoritmos/Iniciais/6 - sextoAlg/melhorAluno.js
+++ Algoritmos/Iniciais/6 - sextoAlg/melhorAluno.js
@@ -1,13 +1,22 @@
-let aluno = 4
+let contador = 0;
+let aluno;
+let quantAlunos = prompt(`Quantos alunos estão na turma?`);
+let maior = 0;
+let nota = 0;
 
-while (contador <= 10) {
-    console.log(contador, soma)
-        if (contador > maior) {
-            maior = contador
-        }
+while (contador < quantAlunos) {
+    
+    if (aluno == 0) {
+        aluno = prompt(`Insira um Aluno:`);
+        nota = prompt(`Insira a nota do Aluno:`);
+    } else {
+        aluno = prompt(`Insira um outro Aluno:`);
+        nota = prompt(`Insira a nota do Aluno:`);
+    };
+    if (nota > maior) {
+        maior = nota
+    }
     contador++;
-    soma =  contador;
-    soma++;
-
 }
-console.log('aaa', maior)
+
+alert(`O aluno com maior produtividade foi ${aluno} com a nota: ${maior}.`)
